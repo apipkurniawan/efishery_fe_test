104c0391435480102d85c83afc52d365cf350443
Apip <davitaja92@gmail.com>
2023-01-04 09:16:53 +0700

    create form

--- src/components/fish/FishForm.tsx
+++ src/components/fish/FishForm.tsx
@@ -1,9 +1,34 @@
 import Modal from "../UI/Modal";
+import "./FishForm.scss";
 
 const FishForm: React.FC<{ onClose: () => void }> = (props) => {
+  const submitHandler = () => {};
+
   return (
     <Modal onClose={props.onClose}>
-      <h3>Form</h3>
+      <form onSubmit={submitHandler} className="form-container">
+        <div className={`control ${false ? "invalid" : ""}`}>
+          <label htmlFor="komoditas">Komoditas</label>
+          <input type="text" id="komoditas" />
+        </div>
+        <div className={`control ${false ? "invalid" : ""}`}>
+          <label htmlFor="prov">City</label>
+          <input type="text" id="prov" />
+        </div>
+        <div className={`control ${false ? "invalid" : ""}`}>
+          <label htmlFor="komoditas">Provinsi</label>
+          <input type="text" id="komoditas" />
+        </div>
+        <div className={`control ${false ? "invalid" : ""}`}>
+          <label htmlFor="prov">Price</label>
+          <input type="text" id="prov" />
+        </div>
+        <div className="actions">
+          <button type="submit" className="btn">
+            Submit
+          </button>
+        </div>
+      </form>
     </Modal>
   );
 };
