f97dc508fafb34a5cb7573f337aaf87f3cd738de
Apip <davitaja92@gmail.com>
2023-01-04 07:54:42 +0700

    change thumbnail

--- src/components/fish/FishThumbnail.tsx
+++ src/components/fish/FishThumbnail.tsx
@@ -3,19 +3,18 @@ import "./FishThumbnail.scss";
 const FishThumbnail = () => {
   return (
     <section className="thumb">
-      <h2>Beri Makan Ikan untuk Dunia</h2>
+      <h2>Feed the Fish for the World</h2>
       <p>
-        Pandemi menyebabkan dampak berkepanjangan pada ketahanan pangan global.
-        Sekitar 660 juta orang masih akan menghadapi isu kelaparan di tahun
-        2030. Akuakultur, sebagai sektor bisnis dengan pertumbuhan yang pesat,
-        memiliki potensi untuk berkontribusi secara signifikan terhadap
-        ketahanan pangan. eFishery percaya bahwa “The Future is Aquaculture”.
+        The pandemic has had a lasting impact on global food security. Around
+        660 million people will still face the issue of hunger in 2030.
+        Aquaculture, as a fast-growing business sector, has the potential to
+        contribute significantly to food security. FishFarmer believes that "The
+        Future is Aquaculture".
       </p>
       <p>
-        Menggunakan kemajuan teknologi, eFishery membangun value chain yang
-        sistematis untuk bisnis budidaya ikan dan udang, menghasilkan ekosistem
-        yang terintegrasi dan berkelanjutan untuk memperkuat ketahanan pangan
-        global di masa mendatang.
+        Using advances in technology, FishFarmer builds a systematic value chain
+        for the fish and shrimp farming business, resulting in an integrated and
+        sustainable ecosystem to strengthen global food security in the future.
       </p>
     </section>
   );
